433ee78a519e45c807d0e674f3289df6c0a0a0b8
alejandroere777 <alejandroere_777@hotmail.com>
2023-11-08 17:40:34 -0300

    2023-11-03

---
+++ app.js
@@ -0,0 +1,15 @@
+const exp = require('constants');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+//app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3000, ()=>console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
